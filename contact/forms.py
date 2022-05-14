4d01cbe97faa01504776f8e36c709b6e5ae9a15b
Chao Ye <yechao@live.cn>
2022-05-14 19:48:52 +0800

    continued

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,7 @@
+from django import forms
 from django.forms import ModelForm
 from .models import Contact
-
+from django import forms
 
 class ContactForm(ModelForm):
     class Meta:
--- contact/models.py
+++ contact/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
-
 class Contact(models.Model):
-    email = models.EmailField()
-    po_number = models.CharField(max_length=10, default='')
+    email = models.EmailField(verbose_name='下单人邮箱地址')
+    po_number = models.CharField(max_length=10, verbose_name='销售单号（7开头）')
+    client=models.CharField(max_length=20, verbose_name='客户', blank=True)
+    salesperson=models.CharField(max_length=10, verbose_name='销售人员', blank=True)
+
+    offices=[
+        ('bj', '北京'),
+        ('sh', '上海'),
+        ('gz', '广州'),
+        ('cd', '成都')
+    ]
+    office=models.CharField(max_length=20, choices=offices, verbose_name='分公司', blank=True)
 
     def __str__(self):
         return self.email
